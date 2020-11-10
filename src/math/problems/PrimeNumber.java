aca7d22bb4c153950d50f5449edc9f7351fccb1f
rkmissile <rkmissile@rocketmail.com>
2020-11-10 11:36:33 -0500

    S10-PrimeNumber

--- src/math/problems/PrimeNumber.java
+++ src/math/problems/PrimeNumber.java
@@ -13,6 +13,30 @@ public class PrimeNumber {
          *
          */
 
+        System.out.println("Prime number from 2 to 1 million: ");
+        for (int i = 2; i <= 1000000; i++) {
+            if (isPrime(i)) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static boolean isPrime(int num) {
+        if (num == 2 || num == 3) {
+            return true;
+        }
+        if (num % 2 == 0 || num % 3 == 0) {
+            return false;
+        }
+        for (int i = 3; i < Math.sqrt(num); i += 2) {
+            if (num % i == 0 || num % Math.sqrt(num) == 0) {
+                return false;
+            }
+        }
+        return true;
+
     }
 
 }
+
+
