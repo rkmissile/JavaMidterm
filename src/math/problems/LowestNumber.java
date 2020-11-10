21a486f01beba5e2f0f4fe97434fe5f0a2cffc0c
rkmissile <rkmissile@rocketmail.com>
2020-11-10 10:20:54 -0500

    S8-LowesrNumber

--- src/math/problems/LowestNumber.java
+++ src/math/problems/LowestNumber.java
@@ -16,19 +16,27 @@ public class LowestNumber {
 
         //find lowest number from the array
 
-        ConnectToSqlDB connectToSqlDB = new ConnectToSqlDB();
-        List<String> lowestValue = new ArrayList<String>();
-        try {
-            connectToSqlDB.insertDataFromArrayToSqlTable(array, "tbl_lowestNumber", "column_lowestNumber");
-            lowestValue = connectToSqlDB.readDataBase("tbl_lowestNumber", "column_lowestNumber");
+        int min=array[0];
+        for(int x:array){
+            if(x<min){
+                min=x;
+            }
+        }
+        System.out.println("Minimum number is "+min);
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        System.out.println("Data is reading from the Table (tbl_primenumber) and displaying to the console");
-        for (String st : lowestValue) {
-            System.out.println(st);
-        }
+//        ConnectToSqlDB connectToSqlDB = new ConnectToSqlDB();
+//        List<String> lowestValue = new ArrayList<String>();
+//        try {
+//            connectToSqlDB.insertDataFromArrayToSqlTable(array, "tbl_lowestNumber", "column_lowestNumber");
+//            lowestValue = connectToSqlDB.readDataBase("tbl_lowestNumber", "column_lowestNumber");
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        System.out.println("Data is reading from the Table (tbl_primenumber) and displaying to the console");
+//        for (String st : lowestValue) {
+//            System.out.println(st);
+//        }
     }
 
 }
