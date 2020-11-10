2b79c8d582b156eb8004e74a1837fa7a913328b1
rkmissile <rkmissile@rocketmail.com>
2020-11-10 09:13:54 -0500

    S4-Factorial

--- src/math/problems/Factorial.java
+++ src/math/problems/Factorial.java
@@ -9,5 +9,39 @@ public class Factorial {
          *
          */
 
+        // recursion way to get factorial of numbers>>
+        int factorial= factRecursion(5);
+        System.out.println("Factorial of given n in recursion way is: "+factorial);
+
+        //iterator way to get factorial of numbers>>>
+        factIteration();
+
+
+
+    }
+
+
+    static int factRecursion(int n)
+    {
+        int output;
+        if(n==1){
+            return 1;
+        }
+        //Recursion: Function calling itself!!
+        output = factRecursion(n-1)* n;
+        return output;
+    }
+
+
+    static void factIteration(){
+
+        int fact=1;
+        int number=5;
+
+        for(int i=1;i<=number;i++){
+            fact=fact*i;
+        }
+        System.out.println("Factorial of "+number+" in iterator way is: "+fact);
+
     }
 }
